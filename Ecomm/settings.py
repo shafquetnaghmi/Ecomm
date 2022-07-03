67dadda1a12b464523d851af3f93b1805cca1415
Shafquet Naghmi <shafquetnaghmi@hotmail.com>
2022-07-03 18:50:07 +0530

    added static

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
